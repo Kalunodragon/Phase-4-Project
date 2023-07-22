495be5414c4d3702ecf157a135a1c1e9dc955849
Andrew <andrewonulak@gmail.com>
2023-07-22 16:34:39 -0600

    Added switchable edit form to the Profile section

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,13 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { userContext } from "./App";
 
 function Profile(){
   const user = useContext(userContext)
+  const [showEditForm, setShowEditForm] = useState(false)
+
+  function handleEditButton(){
+    setShowEditForm(v => !v)
+  } 
 
   return(
-    <>
-      <h1>{user.first_name}'s Profile</h1>
-    </>
+    <div>
+      {showEditForm ?  
+        <button onClick={handleEditButton}>Exit Edit</button> :
+        <button onClick={handleEditButton}>Edit Profile</button>
+      }
+      {showEditForm ?
+        <>
+          <h3>EDITING USER PROFILE</h3>
+          <form>
+            <input
+              type='text'
+              name='user_name'
+              // value={formData.user_name}
+              // onChange={handleChange}
+              placeholder="Username">
+            </input>
+          </form>
+        </>:
+        <>
+          <h3>User Profile</h3>
+          <h1>{user.first_name}'s Profile</h1>
+        </>
+      }
+    </div>
   )
 }
 
