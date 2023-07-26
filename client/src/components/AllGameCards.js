6a85673594c173555d409580fe79795137f96580
Andrew <andrewonulak@gmail.com>
2023-07-26 13:50:50 -0600

    Setting up games components with api and initial get fetch

--- client/src/components/AllGameCards.js
+++ client/src/components/AllGameCards.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function AllGameCards(){
+  const [games, setGames] = useState(null)
+  const [gameCheck, setGameCheck] = useState(false)
+
+  useEffect(()=>{
+    fetch('/game')
+    .then((res) =>{
+      if(res.ok){
+        res.json()
+        .then((d)=>{
+          console.log(d)
+          setGames(d)
+          setGameCheck(true)
+        })
+      } else {
+        setGameCheck(true)
+        console.log("there was an error checking the games")
+      }
+    })
+    .catch(err => console.log(err))
+  },[])
+
+  if(!gameCheck){
+    return(
+      <h1>Loading...</h1>
+    )
+  }
 
   return(
     <h1>List of game card components</h1>
