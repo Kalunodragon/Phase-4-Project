7f0aba08bb5ae38ca43a6a82e4c52891ec8a77c7
Andrew <andrewonulak@gmail.com>
2023-07-26 15:06:35 -0600

    Filling in each GameCard

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function GameCard({ game }){
 
-console.log(game)
+console.log(game.game_title)
 
   return(
     <div>
-      <h3>Title</h3>
-      <h4>Platform</h4>
-      <p>release year and exclusive</p>
+      <h3>Title: {game.game_title}</h3>
+      {game.exclusive ? <p>Platform exclusive</p> : null}
+      <h4>Platform: {game.platform}</h4>
+      <p>release year: {game.release_year}</p>
     </div>
   )
 }
