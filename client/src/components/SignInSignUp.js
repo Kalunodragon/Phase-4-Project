4eb31a297580f732742dadef3244967add8bc49b
Andrew <andrewonulak@gmail.com>
2023-07-28 15:05:27 -0600

    Sign in sign up component initial set up

---
+++ client/src/components/SignInSignUp.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SignUpForm";
+
+function SignInSignUp({setUserLogIn}){
+  const [change, setChange] = useState(false)
+
+  function handleButtonClick(){
+    setChange(v => !v)
+  }
+
+  return(
+    <>
+      <h1>Sign-{change ? "in" : "up"}</h1>
+      {change ? <SignInForm setUserLogIn={setUserLogIn}/> : <SignUpForm />}
+      <button onClick={handleButtonClick}>Switch to Sign-{change ? "up" : "in"}</button>
+    </>
+  )
+}
+
+export default SignInSignUp
