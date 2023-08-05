2cd403b5dda0ad60fd773b2b0b379b636672ead2
Andrew <andrewonulak@gmail.com>
2023-08-05 15:02:01 -0600

    Edit to remove error in file

--- client/src/components/ReviewEditForm.js
+++ client/src/components/ReviewEditForm.js
@@ -3,6 +3,8 @@ import React from "react";
 function ReviewEditForm(){
 
   return(
+    <>
+    </>
     // Set up a form that is prefilled with review info that already exsists, then edit that form and resubmit it
   )
 }
